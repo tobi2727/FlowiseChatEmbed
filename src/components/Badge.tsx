0c3eaaf51a422ed7da375b0d08583457ff666edd
tobi2727 <42752021+tobi2727@users.noreply.github.com>
2023-10-03 17:46:17 +0200

    Update Badge.tsx
    .

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -61,7 +61,7 @@ export const Badge = (props: Props) => {
         id="lite-badge"
         style={{ "font-weight": 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> Talentchat</span>
+        <span> Talentchat!</span>
       </a>
     </span>
     <span style={{
